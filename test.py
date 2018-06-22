ceae603d08867a790d47b14f3d37e71842b47e1a
melody2ma <40501407+melody2ma@users.noreply.github.com>
2018-06-22 13:02:09 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+num1 = 0
+num2 = 0
+
+
+num1 = (input("Give us a number: "))
+num2 = (input("Give us another number: "))
+
+difference = num1 - num2
+
+print("Here is the difference: %d" %difference)
